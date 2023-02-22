11f48f4184ee37564ef9070d1a16f0d369f29839
hafidz313 <90201296+hafidz313@users.noreply.github.com>
2023-02-22 09:02:41 +0700

    Add files via upload

---
+++ __manifest_.py
@@ -0,0 +1,21 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "Author"        : "Hafidz Irwan M",
+    "website"       : "https://www.kompas.com",
+    "category"      : "New Modul",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """Latihan Membuat addons baru di odoo 1""",
+    "depends"       : ["base"],
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "images"        : [],
+    "qweb"          : [],
+    "css"           : [],
+    
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+    
+}
